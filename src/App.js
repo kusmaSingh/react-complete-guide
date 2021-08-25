61f61a30a9ac1217acdbae3b4660eb67bf2317e5
kusmaSingh <kusma.singh04@gmail.com>
2021-08-25 22:13:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import Expenses from './components/Expenses';
+function App() {
+  const expensees =[
+    {id:'e1' , title : 'Car insurance', amount: 799.49, date: new Date(2021,3,21)},
+    {id:'e2', title : 'Mobile insurance', amount: 245.49, date: new Date(2021,4,1)},
+    {id:'e3', title : 'New Desk (wooden)', amount: 199.49, date: new Date(2021,5,25)},
+    {id:'e4', title : 'Motor bike', amount: 399.49, date: new Date(2021,2,2)},
+    {id:'e5', title : 'Toilet paper' ,amount: 499.49, date: new Date(2021,6,20)},
+    {id:'e6', title : 'New TV' ,amount: 499.49, date: new Date(2021,6,20)}
+
+  ]
+  return (
+    <div className="App">
+      <h2>Let's get started!!</h2>
+<Expenses items={expensees}></Expenses>
+    
+    </div>
+  );
+}
+
+export default App;
