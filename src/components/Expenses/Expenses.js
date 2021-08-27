2adb3ac4da1bd732f4cd757d8b2ab30fc362f458
kusmaSingh <kusma.singh04@gmail.com>
2021-08-27 16:59:51 +0530

    static expense changes into dynamic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,37 +1,27 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import ExpenseFilter from './ExpenseFilter';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filterYear, seteFilterYear] = useState('2020');
+  const filterCHangeHandler = selectedYear =>{
+    seteFilterYear(selectedYear)
+  }
+  
   return (
     <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      <ExpenseFilter selected={filterYear} onChangeFilter ={filterCHangeHandler}></ExpenseFilter>
+     {props.items.map((expense) => (
        <ExpenseItem
-        title={props.items[4].title}
-        amount={props.items[4].amount}
-        date={props.items[4].date}
-      />
+       title={expense.title}
+       amount={expense.amount}
+       date={expense.date}
+       key={expense.id}
+     />
+     ))}
+      
     </Card>
   );
 }
